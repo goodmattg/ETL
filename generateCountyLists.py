35d21e30579db0f58fe500534b992208eb4466b0
goodmattg <mattgoodman13@gmail.com>
2016-11-12 14:52:22 -0500

    updated code to clean presidential data

--- generateCountyLists.py
+++ generateCountyLists.py
@@ -7,7 +7,22 @@ Code to generate county lists
 
 '''
 
-base_dir = 'Raw_Returns/Election_Returns_';
+abbrevMap = {}
+
+def uppercaseString(str):
+    return str.upper()
+
+def dictTransform(rowVal, dict):
+    return dict[rowVal]
+
+# build the abbreviation transform map
+abbrevs = pd.read_csv('Mappings/stateAbbrevToExpanded.csv')
+for index, row in abbrevs.iterrows():
+    abbrevMap[row['Abbrev']] = row['State']
+
+
+base_dir = 'PresidentialReturns_RAW/Election_Returns_'
+out_dir = 'PresidentialReturns_CLEAN/Election_Returns_'
 file_prefix = 'returns_'
 template = Template('$dir$year/$fname$year.csv')
 range = [1984, 1988, 1992, 1996, 2000, 2004, 2008, 2012]
@@ -15,6 +30,12 @@ countyLists = {}
 
 for y in range:
     abs_path = template.substitute(dir=base_dir,fname=file_prefix,year=y)
+    out_path = template.substitute(dir=out_dir,fname=file_prefix,year=y)
     print('Processing: {:s}'.format(abs_path))
     data = pd.read_csv(abs_path,header=4)
-    print(data['County'].shape)
+    # Map state abbrev to state name
+    data['State'] = data['State'].map(abbrevMap)
+    # Remove non-data entry rows
+    data = data.dropna(thresh=1)
+    data = data.sort_values('State',axis=0)
+    data.to_csv(out_path)
