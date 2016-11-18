20f63609a0421fc1d6b9ce393ba9bdc72a8f4b36
goodmattg <mattgoodman13@gmail.com>
2016-11-18 14:40:37 -0500

    finished clean file. Added global clean checksum

--- cleanMaster.py
+++ cleanMaster.py
@@ -1,6 +1,7 @@
 import helpers as hp
 from string import Template
 import pandas as pd
+import numpy as np
 
 '''
 Master file to generate "clean" data from datasets defined in
@@ -22,80 +23,45 @@ TR_DIR = config['dirHeaders']['transformed_dir']
 DATA_FTYPE = config['dataFileType']
 
 # define template for the absolute file path (defined in config.yaml)
-sf_template = Template('$base/$type/$fname$ftype')
-mf_template = Template('$base/$type/$fname$year$ftype')
+sf_template = Template('$base$type$fname$ftype')
+mf_template = Template('$base$type$fname$year$ftype')
 
 # load the state abbreviation map
 abbrevMap = hp.getAbbreviationMap()
 
+f_checksum = open('checksum_CLEAN.txt', 'w')
+
+
 for s in config['datasets']:
-    ds = s['set']
-    print("Processing dataset: {:s}\n".format(ds['name']))
+    try:
 
-    if ds['single_file']:
-        absPath = sf_template.substitute(base=ds['directory'],
-                                      type=RAW_DIR,
-                                      fname=ds['file_base'],
-                                      ftype=DATA_FTYPE)
+        ds = s['set']
+        print("Processing dataset: {:s}".format(ds['name']))
 
-        outPath = sf_template.substitute(base=ds['directory'],
-                                      type=CLEAN_DIR,
-                                      fname=ds['file_base'],
-                                      ftype=DATA_FTYPE)
+        if ds['single_file']:
+            absPath = sf_template.substitute(base=ds['directory'],
+                                          type=RAW_DIR,
+                                          fname=ds['file_base'],
+                                          ftype=DATA_FTYPE)
 
-        print("Loading: {:s}\n".format(absPath))
-        rawData = pd.read_csv(absPath)
+            outPath = sf_template.substitute(base=ds['directory'],
+                                          type=CLEAN_DIR,
+                                          fname=ds['file_base'],
+                                          ftype=DATA_FTYPE)
 
-        if ds['loc_single_column']:
-            # split the single column
-            for idx, row in rawData.iterrows():
-                tmp = row['Location'].split(',')
-
-                if (len(tmp) > 1):
-                    row['State'] = tmp[1].upper().strip()
-                    row['County'] = tmp[0].upper().strip()
-                else:
-                    # matches either STATE, UNITED STATES, District of Columbia
-                    row['County'] = tmp[0].upper().strip()
-                    row['State'] = "z_NA"
-                rawData.loc[idx] = row
-
-        else:
-            # data is already stored in "State" and "County"
-            for idx, row in rawData.iterrows():
-                row['State'] = row['State'].upper().strip()
-                row['County'] = row['County'].upper().strip()
-                rawData.loc[idx] = row
-
-        rawData['State'] = rawData['State'].map(abbrevMap, na_action='ignore')
-        rawData = rawData.sort_values(['State', 'County'], axis=0)
-        rawData.to_csv('{:s}'.format(outPath))
-
-    else:
-        # Handle multiple files
-        for year in hp.yearList(config['year_start'],
-                             config['year_end'],
-                             config['year_increment'],
-                             config['years_absent']):
-
-            absPath = template.substitute(base=ds['directory'],
-                                  type=RAW_DIR,
-                                  fname=ds['file_base'],
-                                  year=year,
-                                  ftype=DATA_FTYPE)
-
-            outPath = template.substitute(base=ds['directory'],
-                                  type=CLEAN_DIR,
-                                  fname=ds['file_base'],
-                                  year=year,
-                                  ftype=DATA_FTYPE)
-
+            print("Loading: {:s}".format(absPath))
             rawData = pd.read_csv(absPath)
 
-            if ds['loc_single_column']:
-                # split the single column
+            if ds['loc_single_column']: # split the single column
+
+                # Add 'State' column
+                rawData['State'] = np.zeros(rawData.shape[0])
+                rawData = rawData.rename(columns = {'Location': 'County'})
+                # county value must be non-null
+                rawData = rawData.dropna(thresh=1, subset=['County'])
+
                 for idx, row in rawData.iterrows():
-                    tmp = row['Location'].split(',')
+                    tmp = row['County'].split(',')
 
                     if (len(tmp) > 1):
                         row['State'] = tmp[1].upper().strip()
@@ -106,8 +72,11 @@ for s in config['datasets']:
                         row['State'] = "z_NA"
                     rawData.loc[idx] = row
 
-            else:
-                # data is already stored in "State" and "County"
+            else: # data is already stored in "State" and "County"
+
+                # county AND state value must be non-null
+                rawData = rawData.dropna(thresh=2, subset=['County', 'State'])
+
                 for idx, row in rawData.iterrows():
                     row['State'] = row['State'].upper().strip()
                     row['County'] = row['County'].upper().strip()
@@ -115,8 +84,71 @@ for s in config['datasets']:
 
             rawData['State'] = rawData['State'].map(abbrevMap, na_action='ignore')
             rawData = rawData.sort_values(['State', 'County'], axis=0)
-            rawData.to_csv('{:s}'.format(outPath))
+            print("{:s} cleaned. Outputting to: {:s}".format(ds['name'], outPath))
+            rawData.to_csv(outPath)
 
 
+        else:
+            # Handle multiple files
+            for year in hp.yearList(ds['year_start'],
+                                 ds['year_end'],
+                                 ds['year_increment'],
+                                 ds['years_absent']):
 
+                absPath = mf_template.substitute(base=ds['directory'],
+                                      type=RAW_DIR,
+                                      fname=ds['file_base'],
+                                      year=year,
+                                      ftype=DATA_FTYPE)
+
+                outPath = mf_template.substitute(base=ds['directory'],
+                                      type=CLEAN_DIR,
+                                      fname=ds['file_base'],
+                                      year=year,
+                                      ftype=DATA_FTYPE)
+
+                print("Loading: {:s}".format(absPath))
+                rawData = pd.read_csv(absPath)
+
+                if ds['loc_single_column']:
+                    # add 'State' column
+                    rawData['State'] = np.zeros(rawData.shape[0])
+                    rawData = rawData.rename(columns = {'Location': 'County'})
+                    # county value must be non-null
+                    rawData = rawData.dropna(thresh=1, subset=['County'])
+
+                    # split the single column
+                    for idx, row in rawData.iterrows():
+                        tmp = row['County'].split(',')
+
+                        if (len(tmp) > 1):
+                            row['State'] = tmp[1].upper().strip()
+                            row['County'] = tmp[0].upper().strip()
+                        else:
+                            # matches either STATE, UNITED STATES, District of Columbia
+                            row['County'] = tmp[0].upper().strip()
+                            row['State'] = "z_NA"
+                        rawData.loc[idx] = row
+
+                else:
+                    # county AND state value must be non-null
+                    rawData = rawData.dropna(thresh=2, subset=['County', 'State'])
+
+                    # data is already stored in "State" and "County"
+                    for idx, row in rawData.iterrows():
+                        row['State'] = row['State'].upper().strip()
+                        row['County'] = row['County'].upper().strip()
+                        rawData.loc[idx] = row
+
+                rawData['State'] = rawData['State'].map(abbrevMap, na_action='ignore')
+                rawData = rawData.sort_values(['State', 'County'], axis=0)
+                print("{:s} cleaned. Outputting to: {:s}".format(ds['name'], outPath))
+                rawData.to_csv(outPath)
+
+        f_checksum.write("Finished cleaning dataset: {:s}\n".format(ds['name']))
+
+    except:
+        f_checksum.write("ERROR cleaning dataset: {:s}\n".format(ds['name']))
+
+f_checksum.close()
 
