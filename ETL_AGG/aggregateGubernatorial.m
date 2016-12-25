01398e00a38aec26d65b999c0fa167d79fac49ae
goodmattg <mattgoodman13@gmail.com>
2016-12-25 10:44:28 -0600

    agg scripts added

---
+++ ETL_AGG/aggregateGubernatorial.m
@@ -0,0 +1,46 @@
+clc; clear all; 
+
+dataVectorSize = getCityListSize();
+
+y_start = 2008; 
+y_end = 2015; 
+y_inc = 1; 
+
+completeDemocrat = zeros(dataVectorSize, length(y_start:y_inc:y_end));
+completeRepublican = zeros(dataVectorSize, length(y_start:y_inc:y_end));
+demVotes = zeros(dataVectorSize, length(y_start:y_inc:y_end));
+repVotes = zeros(dataVectorSize, length(y_start:y_inc:y_end));
+
+totalVotes = zeros(dataVectorSize, length(y_start:y_inc:y_end));
+
+for y = y_start:y_inc:y_end
+    idx = (y-y_start)/y_inc+1;
+    fname = '../GovernorReturns/TR/returnsGubernatorial_';
+    fileID = fopen( strcat(fname,int2str(y),'.csv'));
+    rawData = textscan(fileID,'%s %s %d %d', 'Delimiter', ',','HeaderLines',1);
+    dem = double(rawData{3});
+    rep = double(rawData{4});
+    tot = double(dem + rep);
+    pDem = dem ./ tot; 
+    pRep = rep ./ tot;
+    completeDemocrat(:,idx) = pDem;
+    completeRepublican(:,idx) = pRep;
+    demVotes(:,idx) = dem;
+    repVotes(:,idx) = rep;
+    totalVotes(:,idx) = tot;
+    fclose(fileID); 
+end
+
+completeRepublican(isnan(completeRepublican) | (completeRepublican == 1)) = 0;
+completeDemocrat(isnan(completeDemocrat) | (completeDemocrat == 1)) = 0;
+
+govData = struct('y_start',y_start,...
+                  'y_end',y_end, ...
+                  'y_increment',y_inc, ...
+                  'demPerc',completeDemocrat, ...
+                  'repPerc',completeRepublican, ...
+                  'totalVotes', totalVotes, ...
+                  'States', rawData(1), ...
+                  'Counties',rawData(2));
+
+save('../GovernorReturns/MAT/gubernatorialReturns.mat', 'govData')
